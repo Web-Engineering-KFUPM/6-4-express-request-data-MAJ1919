c00c3fc8e1debff320534013fc5022289354c44a
MAJ1919 <s202271080@kfupm.edu.sa>
2026-04-08 10:29:47 +0300

    fix the tasks for test 2 and 3

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -107,23 +107,24 @@ LAB SETUP INSTRUCTIONS
 //import express.
 import express from "express";
 
-
-// create express app instance to create web server
 const app = express();
-app.listen(3000, () => console.log("API running at http://localhost:3000"));
-
 
 // Query params: /echo?name=Ali&age=22
-app.get("/echo", (req,res)=>{ name = req.query.name; age = req.query.age;
-   const { name, age } = req.query;
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
   if (!name || !age) {
-    res.status(400).json({ ok:false, error:"name & age required" });
-  } else {
-    res.json({ ok:true, name, age, msg:`Hello ${name}, you are ${age}` });
-  }
+    return res.status(400).json({ ok: false, error: "name & age required" });
+  } 
+  
+  res.json({ 
+    ok: true, 
+    name, 
+    age, 
+    msg: `Hello ${name}, you are ${age}` 
+  });
 });
 
- 
 // Route params: /profile/First/Last
 app.get('/profile/:first/:last', (req, res) => {
     const { first, last } = req.params;
@@ -132,6 +133,9 @@ app.get('/profile/:first/:last', (req, res) => {
         fullName: `${first} ${last}`
     });
 });
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
 
 // Route param middleware example: /users/42
 app.param('userId', (req, res, next, userId) => {
